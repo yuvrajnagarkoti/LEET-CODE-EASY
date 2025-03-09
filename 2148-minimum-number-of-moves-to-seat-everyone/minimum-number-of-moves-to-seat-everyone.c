fa11da83191f94427d202996494b5c66044fbb7e
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-09 12:38:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (55.95%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.c
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.c
@@ -1,33 +1,18 @@
+// Comparison function for qsort
+int compare(const void *a, const void *b)
+{
+    return (*(int*)a - *(int*)b);
+}
+
 int minMovesToSeat(int* seats, int n1, int* students, int n2)
 {
-    int i,j,temp,min=0;
-    for(i=0;i<n1;i++)
-    {
-        for(j=i+1;j<n1;j++)
-        {
-            if(seats[i]>seats[j])
-            {
-                temp=seats[i];
-                seats[i]=seats[j];
-                seats[j]=temp;
-            }
-        }
+    int minMoves = 0;
+    // Sorting both arrays using qsort (O(n log n))
+    qsort(seats, n1, sizeof(int), compare);
+    qsort(students, n2, sizeof(int), compare);
+    // Calculating minimum moves
+    for (int i = 0; i < n1; i++) {
+        minMoves += abs(seats[i] - students[i]);
     }
-    for(i=0;i<n2;i++)
-    {
-        for(j=i+1;j<n2;j++)
-        {
-            if(students[i]>students[j])
-            {
-                temp=students[i];
-                students[i]=students[j];
-                students[j]=temp;
-            }
-        }
-    }
-    for(i=0;i<n1;i++)
-    {
-        min= min + abs(students[i]-seats[i]);
-    }
-    return min;
+    return minMoves;
 }
