dd866551aea0017aa37ec8c3fd4b6893bedc847e
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-09 18:32:15 +0530

    Time: 5 ms (26.19%) | Memory: 8.7 MB (55.95%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.c
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.c
@@ -1,15 +1,54 @@
-// Comparison function for qsort
-int compare(const void *a, const void *b)
+void merge(int a[],int low,int mid,int high)
 {
-    return (*(int*)a - *(int*)b);
+    int i,j,k;
+    int n1=(mid-low+1);
+    int n2=(high-mid);
+    int leftarr[n1],rightarr[n2];
+    for(i=0;i<n1;i++)
+        leftarr[i]=a[low+i];
+    for(j=0;j<n2;j++)
+        rightarr[j]=a[mid+1+j];
+    i=j=0;
+    k=low;
+    while(i<n1 && j<n2)
+    {
+        if(leftarr[i]<=rightarr[j])
+        {
+            a[k]=leftarr[i++];
+        }
+        else
+        {
+            a[k]=rightarr[j++];
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        a[k++] = leftarr[i++];
+    }
+    while (j < n2)
+    {
+        a[k++] = rightarr[j++];
+    }
+}
+
+void mergesort(int a[],int low,int high)
+{
+    if(low<high)
+    {
+        int mid = low + (high-low)/2;
+        mergesort(a,low,mid);
+        mergesort(a,mid+1,high);
+        merge(a,low,mid,high);
+    }
 }
 
 int minMovesToSeat(int* seats, int n1, int* students, int n2)
 {
     int minMoves = 0;
-    // Sorting both arrays using qsort (O(n log n))
-    qsort(seats, n1, sizeof(int), compare);
-    qsort(students, n2, sizeof(int), compare);
+    // Sorting both arrays using mergesort (O(n log n))
+    mergesort(seats,0,n1-1);
+    mergesort(students,0,n2-1);
     // Calculating minimum moves
     for (int i = 0; i < n1; i++) {
         minMoves += abs(seats[i] - students[i]);
