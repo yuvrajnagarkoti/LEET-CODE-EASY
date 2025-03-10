eefcbcf18fde1d6cc9df98273fa0a66590276fd0
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-11 01:05:57 +0530

    Time: 3 ms (67.86%) | Memory: 9.4 MB (78.57%) - LeetSync

--- 2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.c
+++ 2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.c
@@ -1,15 +1,21 @@
-int comp(const void *a,const void *b){
+int comp(const void *a,const void *b)
+{
     return *(int*)b-*(int*)a;
 }
-int deleteGreatestValue(int** grid, int gridSize, int* gridColSize) {
-    for(int i=0;i<gridSize;i++){
+int deleteGreatestValue(int** grid, int gridSize, int* gridColSize)
+{
+    for(int i=0;i<gridSize;i++)
+    {
         qsort(grid[i],gridColSize[i],sizeof(int),comp);
     }
     int sum=0;
-    for(int i=0;i<gridColSize[0];i++){
+    for(int i=0;i<gridColSize[0];i++)
+    {
         int large=0;
-        for(int j=0;j<gridSize;j++){
-            if(grid[j][i]>large){
+        for(int j=0;j<gridSize;j++)
+        {
+            if(grid[j][i]>large)
+            {
                 large=grid[j][i];
             }
         }
