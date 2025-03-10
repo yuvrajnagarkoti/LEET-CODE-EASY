27830e7b75d428df482c6b3d662d3118ae580cd8
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-11 01:02:04 +0530

    Time: 2 ms (72.22%) | Memory: 9.3 MB (77.78%) - LeetSync

--- 2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.c
+++ 2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.c
@@ -1,11 +1,15 @@
-int deleteGreatestValue(int** grid, int n1, int* n2) {
+int deleteGreatestValue(int** grid, int n1, int* n2)
+{
     int i, j, temp;
-    
     // Sort each row in descending order
-    for (int a = 0; a < n1; a++) {
-        for (i = 0; i < n2[a] - 1; i++) {
-            for (j = i + 1; j < n2[a]; j++) {
-                if (grid[a][i] < grid[a][j]) {
+    for (int a = 0; a < n1; a++)
+    {
+        for (i = 0; i < n2[a] - 1; i++)
+        {
+            for (j = i + 1; j < n2[a]; j++)
+            {
+                if (grid[a][i] < grid[a][j])
+                {
                     temp = grid[a][i];
                     grid[a][i] = grid[a][j];
                     grid[a][j] = temp;
@@ -16,15 +20,17 @@ int deleteGreatestValue(int** grid, int n1, int* n2) {
 
     int sum = 0;
     // Find the max element in each column
-    for (i = 0; i < n2[0]; i++) {  // Iterate through columns
+    for (i = 0; i < n2[0]; i++) // Iterate through columns
+    {  
         int max = grid[0][i];
-        for (j = 1; j < n1; j++) {
-            if (grid[j][i] > max) {
+        for (j = 1; j < n1; j++)
+        {
+            if (grid[j][i] > max)
+            {
                 max = grid[j][i];
             }
         }
         sum += max;
     }
-
     return sum;
 }
