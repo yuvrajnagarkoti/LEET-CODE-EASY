d7791b25409cabd111cf1a75d3550e94be0828b5
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-10 09:19:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (81.22%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.c
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.c
@@ -1,42 +1,17 @@
-void merge(int a[],int low,int mid,int high)
-{
-    int i,j,k;
-    int n1=mid-low+1;
-    int n2=high-mid;
-    int leftarr[n1],rightarr[n2];
-    for(i=0;i<n1;i++)
-        leftarr[i]=a[low+i];
-    for(j=0;j<n2;j++)
-        rightarr[j]=a[mid+1+j];
-    i=j=0;
-    k=low;
-    while(i<n1 && j<n2)
-    {
-        if(leftarr[i]<=rightarr[j])
-            a[k++]=leftarr[i++];
-        else
-            a[k++]=rightarr[j++];
-    }
-    while(i<n1)
-        a[k++]=leftarr[i++];
-    while(j<n2)
-        a[k++]=rightarr[j++];
-}
-
-void mergesort(int a[],int low,int high)
-{
-    if(low<high)
-    {
-        int mid = low + (high-low)/2;
-        mergesort(a,low,mid);
-        mergesort(a,mid+1,high);
-        merge(a,low,mid,high);
-    }
-}
-
 int maxProduct(int* nums, int n)
 {
-    mergesort(nums,0,n-1);
-    int max= (nums[n-2]-1)*(nums[n-1]-1);
-    return max;
+    int n1 = 0, n2 = 0; // Store two largest numbers
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] > n1)
+        {
+            n2 = n1; // Previous max becomes second max
+            n1 = nums[i]; // Update max
+        }
+        else if (nums[i] > n2)
+        {
+            n2 = nums[i]; // Update second max
+        }
+    }
+    return (n1 - 1) * (n2 - 1);
 }
