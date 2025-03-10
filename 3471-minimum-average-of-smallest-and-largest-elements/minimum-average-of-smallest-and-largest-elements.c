16a7881ea9f1c5ff9ddf10deb0f0b4177298e4ce
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-10 11:04:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (11.22%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.c
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.c
@@ -1,53 +1,57 @@
 #include <float.h>
-void merge(int a[],int low,int mid,int high)
+
+void merge(int a[], int low, int mid, int high)
 {
-    int i,j,k,n1,n2;
-    n1=mid-low+1;
-    n2=high-mid;
-    int leftarr[n1],rightarr[n2];
-    for(i=0;i<n1;i++)
-        leftarr[i]=a[low+i];
-    for(j=0;j<n2;j++)
-        rightarr[j]=a[mid+1+j];
-    i=j=0;
-    k=low;
-    while(i<n1 && j<n2)
+    int i, j, k, n1, n2;
+    n1 = mid - low + 1;
+    n2 = high - mid;
+    
+    int leftarr[n1], rightarr[n2];  // Temporary arrays
+
+    for (i = 0; i < n1; i++)
+        leftarr[i] = a[low + i];
+    for (j = 0; j < n2; j++)
+        rightarr[j] = a[mid + 1 + j];
+
+    i = j = 0;
+    k = low;
+
+    while (i < n1 && j < n2)
     {
-        if(leftarr[i]<rightarr[j])
-            a[k++]=leftarr[i++];
+        if (leftarr[i] < rightarr[j])
+            a[k++] = leftarr[i++];
         else
-            a[k++]=rightarr[j++];
+            a[k++] = rightarr[j++];
     }
-    while(i<n1)
-        a[k++]=leftarr[i++];
-    while(j<n2)
-        a[k++]=rightarr[j++];
+
+    while (i < n1)
+        a[k++] = leftarr[i++];
+    while (j < n2)
+        a[k++] = rightarr[j++];
 }
 
-void mergesort(int a[],int low,int high)
+void mergesort(int a[], int low, int high)
 {
-    if(low<high)
+    if (low < high)
     {
-        int mid=low + (high-low)/2;
-        mergesort(a,low,mid);
-        mergesort(a,mid+1,high);
-        merge(a,low,mid,high);
+        int mid = low + (high - low) / 2;
+        mergesort(a, low, mid);
+        mergesort(a, mid + 1, high);
+        merge(a, low, mid, high);
     }
 }
-double minimumAverage(int* nums, int n)
+
+double minimumAverage(int *nums, int n)
 {
-    mergesort(nums,0,n-1);
-    double averages[n/2];
-    int i,j,k=0;
-    double min=DBL_MAX;
-    for(i=0,j=n-1; i<j ; i++,j--)
+    mergesort(nums, 0, n - 1);  // Sort the array in ascending order
+    double min = DBL_MAX;  // Initialize min to maximum possible value
+    for (int i = 0, j = n - 1; i < j; i++, j--)
     {
-        averages[k]= (double)(nums[i]+nums[j])/2;
-        if(averages[k]<min)
+        double avg = (double)(nums[i] + nums[j]) / 2;
+        if (avg < min)
         {
-            min=averages[k];
+            min = avg;
         }
-        k++;
     }
     return min;
 }
