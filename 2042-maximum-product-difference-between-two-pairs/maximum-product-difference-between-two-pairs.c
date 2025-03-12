7542eec7107236ad16960270a3a4ecb2ed56f17a
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-12 08:27:13 +0530

    Time: 15 ms (95.05%) | Memory: 9.4 MB (46.53%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.c
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.c
@@ -1,14 +1,21 @@
-int compare(const void *a, const void *b) {
-    return (*(int *)a - *(int *)b);
-}
+int maxProductDifference(int* nums, int numsSize) {
+    int max1 = 0, max2 = 0, min1 = __INT_MAX__, min2 = __INT_MAX__;
+    
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] > max1) {
+            max2 = max1;
+            max1 = nums[i];
+        } else if (nums[i] > max2) {
+            max2 = nums[i];
+        }
 
-int maxProductDifference(int* nums, int numsSize)
-{
-    qsort(nums, numsSize, sizeof(int), compare);
-    int max1 = nums[numsSize - 1];
-    int max2 = nums[numsSize - 2];
-    int min1 = nums[0];
-    int min2 = nums[1];
+        if (nums[i] < min1) {
+            min2 = min1;
+            min1 = nums[i];
+        } else if (nums[i] < min2) {
+            min2 = nums[i];
+        }
+    }
     
     return (max1 * max2) - (min1 * min2);
 }
