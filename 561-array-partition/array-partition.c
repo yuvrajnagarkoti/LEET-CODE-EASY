58ac7d6bf4e7bc49e44a1ce9a45b48cdd374c0c8
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-12 09:52:31 +0530

    Time: 37 ms (5.68%) | Memory: 10 MB (85.93%) - LeetSync

--- 561-array-partition/array-partition.c
+++ 561-array-partition/array-partition.c
@@ -1,45 +1,27 @@
-void merge(int a[],int low,int mid,int high)
-{
-    int i,j,k;
-    int n1=mid-low+1;
-    int n2=high-mid;
-    int leftarr[n1],rightarr[n2];
-    for(i=0;i<n1;i++)
-        leftarr[i]=a[low+i];
-    for(j=0;j<n2;j++)
-        rightarr[j]=a[mid+j+1];
-    i=j=0;
-    k=low;
-    while(i<n1 && j<n2)
-    {
-        if(leftarr[i]<rightarr[j])
-            a[k++]=leftarr[i++];
-        else
-            a[k++]=rightarr[j++];
-    }
-    while(i<n1)
-        a[k++]=leftarr[i++];
-    while(j<n2)
-        a[k++]=rightarr[j++];
-}
-void mergesort(int a[],int low,int high)
-{
-    if(low<high)
-    {
-        int mid = low + (high-low)/2;
-        mergesort(a,low,mid);
-        mergesort(a,mid+1,high);
-        merge(a,low,mid,high);
+#define swap(x,y,t) (t=x,x=y,y=t)
+void Q_sort(int *nums,int left,int right){
+    int base=nums[right];
+    int j=left-1;
+    int temp;
+    if(left<right){
+        for(int i=left;i<right;i++){
+            if(base<nums[i]){
+                j++;
+                swap(nums[j],nums[i],temp);
+            }
+        }
+        swap(nums[j+1],nums[right],temp);
+        Q_sort(nums,left,j);
+        Q_sort(nums,j+2,right);
     }
 }
 
-int arrayPairSum(int* nums, int n)
-{
-    mergesort(nums,0,n-1);
-    int i,sum=0;
-    for(i=0;i<n;i+=2)
-    {
-        sum=sum+nums[i];
+
+int arrayPairSum(int* nums, int numsSize) {
+    Q_sort(nums,0,numsSize-1);
+    int sum=0;
+    for(int i=1;i<numsSize;i=i+2){
+        sum=sum+fmin(nums[i-1],nums[i]);
     }
     return sum;
 }
