7729839e487857155efe0b51a1d3fe1cbf25d835
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-12 09:51:41 +0530

    Time: 35 ms (7.41%) | Memory: 9.9 MB (89.63%) - LeetSync

---
+++ 561-array-partition/array-partition.c
@@ -0,0 +1,45 @@
+void merge(int a[],int low,int mid,int high)
+{
+    int i,j,k;
+    int n1=mid-low+1;
+    int n2=high-mid;
+    int leftarr[n1],rightarr[n2];
+    for(i=0;i<n1;i++)
+        leftarr[i]=a[low+i];
+    for(j=0;j<n2;j++)
+        rightarr[j]=a[mid+j+1];
+    i=j=0;
+    k=low;
+    while(i<n1 && j<n2)
+    {
+        if(leftarr[i]<rightarr[j])
+            a[k++]=leftarr[i++];
+        else
+            a[k++]=rightarr[j++];
+    }
+    while(i<n1)
+        a[k++]=leftarr[i++];
+    while(j<n2)
+        a[k++]=rightarr[j++];
+}
+void mergesort(int a[],int low,int high)
+{
+    if(low<high)
+    {
+        int mid = low + (high-low)/2;
+        mergesort(a,low,mid);
+        mergesort(a,mid+1,high);
+        merge(a,low,mid,high);
+    }
+}
+
+int arrayPairSum(int* nums, int n)
+{
+    mergesort(nums,0,n-1);
+    int i,sum=0;
+    for(i=0;i<n;i+=2)
+    {
+        sum=sum+nums[i];
+    }
+    return sum;
+}
