5ca1c47ed2d95cfbafe7127c821e4370a282372c
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-12 08:26:45 +0530

    Time: 26 ms (56.44%) | Memory: 9.9 MB (18.81%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.c
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.c
@@ -1,42 +1,14 @@
-void merge(int a[],int low,int mid,int high)
-{
-    int i,j,k;
-    int n1=mid-low+1;
-    int n2=high-mid;
-    int leftarr[n1],rightarr[n2];
-    for(i=0;i<n1;i++)
-        leftarr[i]=a[low+i];
-    for(j=0;j<n2;j++)
-        rightarr[j]=a[mid+1+j];
-    i=j=0;
-    k=low;
-    while(i<n1 && j<n2)
-    {
-        if(leftarr[i]<rightarr[j])
-            a[k++]=leftarr[i++];
-        else
-            a[k++]=rightarr[j++];
-    }
-    while(i<n1)
-        a[k++]=leftarr[i++];
-    while(j<n2)
-        a[k++]=rightarr[j++];
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
 }
 
-void mergesort(int a[],int low,int high)
+int maxProductDifference(int* nums, int numsSize)
 {
-    if(low<high)
-    {
-        int mid = low + (high-low)/2;
-        mergesort(a,low,mid);
-        mergesort(a,mid+1,high);
-        merge(a,low,mid,high);
-    }
-}
-
-int maxProductDifference(int* nums, int n)
-{
-    mergesort(nums,0,n-1);
-    int max = (nums[n-1]*nums[n-2]) - (nums[0]*nums[1]);
-    return max;
+    qsort(nums, numsSize, sizeof(int), compare);
+    int max1 = nums[numsSize - 1];
+    int max2 = nums[numsSize - 2];
+    int min1 = nums[0];
+    int min2 = nums[1];
+    
+    return (max1 * max2) - (min1 * min2);
 }
