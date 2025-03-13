982881ac5d276b083a0608d844ef79a73c1c3eb2
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-13 10:32:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (49.67%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.c
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.c
@@ -1,15 +1,14 @@
 bool canBeEqual(int* target, int targetSize, int* arr, int arrSize) {
     int freq[1001] = {0};
-
-    for (int i = 0; i < targetSize; i++) {
+    for (int i = 0; i < targetSize; i++)
+    {
         freq[target[i]]++;
         freq[arr[i]]--;
     }
-
-    for(int i = 0; i < 1001; i++) {
+    for(int i = 0; i < 1001; i++)
+    {
         if (freq[i] != 0)
             return false;
     }
     return true;
-    
 }
