a6817e45cc50f257d4aef0561eeac2de8e68e9e5
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-13 19:31:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (7.04%) - LeetSync

--- 1137-height-checker/height-checker.c
+++ 1137-height-checker/height-checker.c
@@ -1,59 +1,40 @@
-void merge(int a[],int low,int mid,int high)
-{
-    int i,j,k;
-    int n1=(mid-low+1);
-    int n2=(high-mid);
-    int leftarr[n1],rightarr[n2];
-    for(i=0;i<n1;i++)
-        leftarr[i]=a[low+i];
-    for(j=0;j<n2;j++)
-        rightarr[j]=a[mid+j+1];
-    i=j=0;
-    k=low;
-    while(i<n1 && j<n2)
-    {
-        if(leftarr[i]<rightarr[j])
-            a[k++]=leftarr[i++];
-        else
-            a[k++]=rightarr[j++];
-    }
-    while(i<n1)
-    {
-        a[k++]=leftarr[i++];
-    }
-    while(j<n2)
-    {
-        a[k++]=rightarr[j++];
-    }
-}
-
-void mergesort(int a[],int low,int high)
-{
-    if(low<high)
-    {
-        int mid = low + (high-low)/2;
-        mergesort(a,low,mid);
-        mergesort(a,mid+1,high);
-        merge(a,low,mid,high);
-    }
-}
-
 int heightChecker(int* heights, int n)
 {
-    int i,c=0;
-    int *a=(int *)malloc(n*sizeof(int));
-    for(i=0;i<n;i++)
+    int i,j,c=0;
+    int max=heights[0];
+    for(i=1;i<n;i++)
     {
-        a[i]=heights[i];
+        if(max<heights[i])
+            max=heights[i];
     }
-    mergesort(a,0,n-1);
+    int * count = (int *)calloc(max+1,sizeof(int));
     for(i=0;i<n;i++)
     {
-        if(a[i]!=heights[i])
+        count[heights[i]]++;
+    }
+    int * expected = (int *)malloc(n*sizeof(int));
+    i=j=0;
+    while(i<max+1)
+    {
+        if(count[i]>0)
+        {
+            expected[j]=i;
+            count[i]--;
+            j++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(expected[i] != heights[i])
         {
             c++;
         }
     }
-    free(a);
+    free(count);
+    free(expected);
     return c;
 }
