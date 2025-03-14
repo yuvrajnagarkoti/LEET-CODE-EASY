e3f20b769b487fc937ebcfef67e7fb70644fc99a
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-14 10:41:50 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (36.59%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.c
+++ 1970-sorting-the-sentence/sorting-the-sentence.c
@@ -1,5 +1,6 @@
 // Function to reconstruct the original sentence
-char* sortSentence(char* s) {
+char* sortSentence(char* s)
+{
     int len = strlen(s);  // Get the length of the input string
     char* ans = (char*)malloc(sizeof(char) * (len + 1)); // Allocate memory for the sorted sentence
     int count = 0; // Index for inserting characters into `ans`
@@ -23,12 +24,10 @@ char* sortSentence(char* s) {
 
                 // Add a space after each word
                 ans[count++] = ' ';
-
-                break; // Stop searching for this position, move to the next number
+                break;
             }
         }
     }
-
     ans[count - 1] = '\0'; // Replace the last space with a null terminator to form a valid string
     return ans; // Return the sorted sentence
 }
