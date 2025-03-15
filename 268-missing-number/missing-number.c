da14396728d46104ec7c64c165d35fab4e1c84a4
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-15 16:55:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (5.27%) - LeetSync

--- 268-missing-number/missing-number.c
+++ 268-missing-number/missing-number.c
@@ -1,16 +1,33 @@
+void countsort(int a[],int n)
+{
+    int i,k;
+    int *count=(int *)calloc(n+1,sizeof(int));
+    for(i=0;i<n;i++)
+        count[a[i]]++;
+    i=0;
+    k=0;
+    while(i<n+1)
+    {
+        if(count[i]>0)
+        {
+            a[k++]=i;
+        }
+        i++;
+    }
+}
+
 int missingNumber(int* nums, int n)
 {
-    int i,flag;
-    int * a =(int *)calloc(n+1,sizeof(int));
-    for(i=0;i<n;i++)
-        a[nums[i]]++;
-    for(i=0;i<n;i++)
-    {
-        if(a[i] == 0)
-        {
-            flag=i;
-            break;
-        }
+    countsort(nums,n);
+    int low = 0, high = n - 1, mid;  
+    while (low <= high) {
+        mid = low + (high - low) / 2;
+        // If index and value match, missing number is on the right
+        if (nums[mid] == mid)
+            low = mid + 1;
+        else
+            high = mid - 1;
     }
-    return flag;
+    // The missing number is at index 'low'
+    return low;
 }
