31a983c8adb069df800033377e1b4295836ff0e5
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-18 20:44:05 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (76.19%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.c
+++ 2478-longest-nice-subarray/longest-nice-subarray.c
@@ -1,14 +1,18 @@
 int longestNiceSubarray(int* nums, int n) {
-    int left = 0, maxLength = 0;
-    int bitMask = 0; // Tracks OR of the current subarray
-    for (int right = 0; right < n; right++) {
-        // If adding nums[right] violates the condition, shrink from left
-        while ((bitMask & nums[right]) != 0) { 
-            bitMask ^= nums[left]; // Remove nums[left] using XOR
+    int left = 0, maxLength = 0, bitMask = 0;
+
+    for (int right = 0; right < n; right++)
+    {
+        while ((bitMask & nums[right]) != 0) // If there's a conflict
+        { 
+            bitMask ^= nums[left]; // Remove nums[left] from bitMask
             left++;
         }
-        bitMask |= nums[right]; // Add nums[right] to the subarray
-        maxLength = (right - left + 1) > maxLength ? (right - left + 1) : maxLength;
+        bitMask |= nums[right]; // Add nums[right] to bitMask
+        if (right - left + 1 > maxLength)
+        { // Update maxLength
+            maxLength = right - left + 1;
+        }
     }
     return maxLength;
 }
