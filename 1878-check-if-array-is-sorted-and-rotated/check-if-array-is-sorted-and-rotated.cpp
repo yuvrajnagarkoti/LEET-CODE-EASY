58340281cf6313bfc57650f81eb9921e43083e69
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-04-01 21:37:30 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (88.55%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,19 +1,17 @@
 using namespace std;
 class Solution
 {
-public:
-    bool check(vector<int>& nums)
-    {
-        int count = 0, n = nums.size();
-        // Count how many times the order breaks
-        for (int i = 0; i < n; i++)
+    public:
+        bool check(vector<int>& nums)
         {
-            if (nums[i] > nums[(i + 1) % n])
+            int count = 0, n = nums.size();
+            for (int i = 0; i < n; i++)
             {
-                count++;
+                if (nums[i] > nums[(i + 1) % n])
+                {
+                    count++;
+                }
             }
+            return count <= 1;
         }
-        // The array can be sorted and rotated if count <= 1
-        return count <= 1;
-    }
 };
