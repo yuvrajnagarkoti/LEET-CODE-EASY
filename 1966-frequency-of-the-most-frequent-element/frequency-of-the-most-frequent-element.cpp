e70095e95077fc61362ce8b89647131d4394c2cb
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-04-04 18:21:28 +0530

    Time: 44 ms (33.23%) | Memory: 106 MB (30.96%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,19 +1,17 @@
-class Solution
-{
+class Solution {
 public:
-    int maxFrequency(vector<int>& nums, int k)
-    {
+    int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         int left = 0, ans = 0;
-        long curr = 0;
+        long prefixSum = 0;
         for (int right = 0; right < nums.size(); right++)
         {
-            long target = nums[right];
-            curr += target; 
-            while ((right - left + 1) * target - curr > k) {
-                curr -= nums[left];
-                left++;
-            }
+            prefixSum += nums[right];      
+            // The sum required to make all elements in range [left, right] equal to nums[right]
+            while ((long)(right - left + 1) * nums[right] - prefixSum > k)
+            {
+                prefixSum -= nums[left++];
+            }      
             ans = max(ans, right - left + 1);
         }
         return ans;
