1187131a538c702ea2649e23710ea2335d71b0c7
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-04-04 18:30:38 +0530

    Time: 36 ms (69.60%) | Memory: 105.8 MB (91.27%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,50 +1,12 @@
 class Solution
 {
 public:
-
-    void merge(vector<int>& nums,int low,int mid,int high)
-    {
-        int i,j,k;
-        int n1=mid-low+1;
-        int n2=high-mid;
-        vector<int>leftarr(n1);
-        vector<int>rightarr(n2);
-        for(i=0;i<n1;i++)
-            leftarr[i]=nums[low+i];
-        for(j=0;j<n2;j++)
-            rightarr[j]=nums[mid+j+1];
-        i=j=0;
-        k=low;
-        while(i<n1 && j<n2)
-        {
-            if(leftarr[i] < rightarr[j])
-                nums[k++]=leftarr[i++];
-            else
-                nums[k++]=rightarr[j++];
-        }
-        while(i<n1)
-            nums[k++]=leftarr[i++];
-        while(j<n2)
-            nums[k++]=rightarr[j++];
-    }
-
-    void mergesort(vector<int>& nums, int low,int high)
-    {
-        if(low<high)
-        {
-            int mid=low+(high-low)/2;
-            mergesort(nums,low,mid);
-            mergesort(nums,mid+1,high);
-            merge(nums,low,mid,high);
-        }
-    }
-
     int maxFrequency(vector<int>& nums, int k)
     {
-        int left = 0, ans = 0,n=nums.size();
-        mergesort(nums,0,n-1);
+        sort(nums.begin(), nums.end());
+        int left = 0, ans = 0;
         long prefixSum = 0;
-        for (int right = 0; right < n; right++)
+        for (int right = 0; right < nums.size(); right++)
         {
             prefixSum += nums[right];      
             // The sum required to make all elements in range [left, right] equal to nums[right]
