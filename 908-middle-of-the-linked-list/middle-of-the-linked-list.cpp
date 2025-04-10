bc3f2763aec0ccfaf7c7078b5c1960eb2d82bfd0
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-04-10 18:38:12 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (25.84%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head)
+    {
+        vector<int> list;
+        ListNode *temp=head;
+        int count=0;
+        while(temp!=NULL)
+        {
+            count++;
+            temp=temp->next;
+        }
+        temp=head;
+        if(count%2 == 0) count=(count+1)/2;
+        else    count=count/2;
+        while(count--)
+        {
+            temp=temp->next;
+        }
+        return temp;
+    }
+};
