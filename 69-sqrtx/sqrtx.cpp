ef995cd883ab869eb2f2cebff5a073109fc4d714
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-04-27 16:34:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (85.71%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -3,20 +3,17 @@ class Solution
 public:
     int mySqrt(int x)
     {
-        if(x <= 1) return x;
-        int l=1,h=x/2,ans=0;
-        while(l <= h)
+        int l = 0, r = x, ans = 0;
+        while (l <= r)
         {
-            long long  mid = l+ (h-l)/2;
-            if( (mid*mid) == x)
-                return mid;
-            else if( (mid*mid) > x )
-                h=mid-1;
-            else
+            long mid = l + (r - l) / 2;
+            if (mid * mid <= x)
             {
-                l=mid+1;
-                ans=mid;
+                ans = mid;
+                l = mid + 1;
             }
+            else
+                r = mid - 1;
         }
         return ans;
     }
