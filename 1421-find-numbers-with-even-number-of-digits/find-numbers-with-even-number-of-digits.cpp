73f4f122308bc96921f49d667220c8c40414f4c3
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-04-30 12:31:36 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (44.44%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -6,9 +6,7 @@ public:
         int count = 0;
         for (int num : nums)
         {
-            // Handle 0 separately because log10(0) is undefined
             int digits = (num == 0) ? 1 : (int)log10(num) + 1;
-
             if (digits % 2 == 0)
                 count++;
         }
