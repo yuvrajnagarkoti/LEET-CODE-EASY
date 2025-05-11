79c43c5d5eb604a6d73db412369f94280cbd38d8
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-05-11 16:46:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (20.63%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -4,13 +4,19 @@ public:
     {
         if (arr.size() < 3)
             return false;
-        for(int i=0 ;i<arr.size()-2;i++)
-        {
-            if(arr[i]%2 != 0 && arr[i+1]%2 != 0 && arr[i+2]%2 != 0)
+        int count=0;        
+        for(int i =0; i < arr.size() ; i++)
+        {   
+            if (count ==3) return true;
+            if(arr[i]%2!=0)
             {
-                return true;
+                count++; 
+            }
+            else
+            {
+                count =0; 
             }
         }
-        return false;
+        return count>=3? true:false; 
     }
 };
