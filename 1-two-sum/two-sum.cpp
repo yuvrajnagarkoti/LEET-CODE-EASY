e0347d6fd49e8a5e4dc675ecd9abf3b83266d6c8
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-07-01 21:25:51 +0530

    Time: 1 ms (80.21%) | Memory: 15 MB (21.59%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,41 +1,20 @@
 class Solution
 {
 public:
-
-    static bool compare ( pair<int,int> &a, pair<int,int> &b)
+    vector<int> twoSum(vector<int>& nums, int target) 
     {
-        return a.first < b.first;
-    }
-
-    vector<int> twoSum(vector<int>& nums, int target)
-    {
-        vector<pair<int,int>> num;
+        map <int,int> mpp;
         vector<int> ans;
         for(int i=0;i<nums.size();i++)
         {
-            num.push_back({nums[i],i});
-        }
-
-        sort(num.begin(),num.end(),compare);
-        int low = 0,high=num.size()-1;
-
-        while(low < high)
-        {
-            int temp = num[low].first + num[high].first;
-            if(temp == target)
+            int temp = target-nums[i];
+            if(mpp.find(temp)  != mpp.end())
             {
-                ans.push_back(num[low].second);
-                ans.push_back(num[high].second);
-                break;
+                ans.push_back(i);
+                ans.push_back(mpp[temp]); 
+                return ans;
             }
-            else if(temp > target)
-            {
-                high--;
-            }
-            else
-            {
-                low++;
-            }
+            mpp[nums[i]] = i;
         }
         return ans;
     }
