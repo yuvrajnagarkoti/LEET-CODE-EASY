2780b3ada49dc4a0fc72f2f214a8f8b6ef5c162e
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-07-04 12:43:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (62.85%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,49 +1,27 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
     {
-        ListNode* list3 = new ListNode();
-        ListNode* temp = list3;
-        while( list1 != NULL && list2 != NULL)
+        ListNode* dummy = new ListNode();
+        ListNode* tail = dummy;
+        while (list1 && list2)
         {
-            if( list1->val < list2->val)
+            if (list1->val < list2->val)
             {
-                list3->next = new ListNode(list1->val);
-                list1=list1->next;
-                list3=list3->next;
+                tail->next = list1;
+                list1 = list1->next;
             }
             else
             {
-                list3->next = new ListNode(list2->val);
-                list2=list2->next;
-                list3=list3->next;
+                tail->next = list2;
+                list2 = list2->next;
             }
+            tail = tail->next;
         }
 
-        while( list1 != NULL)
-        {
-            list3->next = new ListNode(list1->val);
-            list1=list1->next;
-            list3=list3->next;
-        }
+        // Attach the remaining part
+        tail->next = (list1 != NULL) ? list1 : list2;
 
-        while( list2 != NULL)
-        {
-            list3->next = new ListNode(list2->val);
-            list2=list2->next;
-            list3=list3->next;
-        }
-
-        return temp->next;
+        return dummy->next;
     }
 };
