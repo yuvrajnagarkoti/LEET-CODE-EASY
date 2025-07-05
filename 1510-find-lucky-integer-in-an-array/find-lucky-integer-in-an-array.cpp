b40ed00c81adcd643ef295c01b458890bb66bd57
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-07-05 20:00:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (94.67%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
@@ -1,14 +1,15 @@
 class Solution
 {
 public:
-
+    // Custom compare function for sorting in descending order
     static bool compare(int a, int b)
     {
-        return a > b;  // Descending order
+        return a > b;
     }
 
     int findLucky(vector<int>& arr)
     {
+        // Sort the array in descending order
         sort(arr.begin(), arr.end(), compare);
 
         int i = 0, j = 1;
@@ -16,16 +17,21 @@ public:
         {
             if (arr[i] != arr[j])
             {
+                // If frequency of arr[i] equals its value, return it (lucky number found)
                 if (arr[i] == (j - i))
                     return arr[i];
+
+                // Move to the next unique number
                 i = j;
             }
             j++;
         }
 
+        // Check the last group (needed because last group may not be checked inside the loop)
         if (arr[i] == (j - i))
             return arr[i];
 
+        // If no lucky number found, return -1
         return -1;
     }
 };
