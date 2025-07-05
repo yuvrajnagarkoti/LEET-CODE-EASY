a77b72571dab076e7ca784ce41654a8890a45242
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-07-05 19:57:02 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (94.67%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
@@ -1,19 +1,38 @@
-class Solution {
-public:
+class Solution
+{
+    public:
+
+    static bool compare(int a,int b)
+    {
+        return a>b;
+    }
+
     int findLucky(vector<int>& arr)
     {
-        int i;
-        int count[501] = {0};
-        for(i=0;i<arr.size();i++)
+        sort( arr.begin(),arr.end() ,compare);
+        if(arr.size() == 1)
         {
-            count[arr[i]]++;
+            if(arr[0] == 1)     return 1;
+            return -1;
         }
-
-        for(i=500;i>=1;i--)
+        
+        int i=0,j=1;
+        while( j < arr.size() )
         {
-            if( count[i] == i)
-                return i;
+            if( arr[i] != arr[j] )
+            {
+                if( arr[i] == (j-i) )
+                    return j-i;
+                i=j;
+                j++;
+            }
+            else if(arr[i] == arr[j] )
+            {
+                j++;
+            }
         }
+        if(arr[i] == (j-i))
+            return j-i;
         return -1;
     }
 };
