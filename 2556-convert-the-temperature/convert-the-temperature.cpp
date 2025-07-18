491c446d6f77a08539965ba1dde91a15fddc6694
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-07-18 07:45:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (46.62%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.cpp
+++ 2556-convert-the-temperature/convert-the-temperature.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     vector<double> convertTemperature(double celsius)
     {
-        double k=celsius+273.15;
-        double f=celsius*1.80 + 32.00;
-        return {k,f};
+        return { (celsius+273.15) , (celsius*1.80+32.00) };
     }
 };
