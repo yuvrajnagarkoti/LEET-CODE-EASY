043803ba608c0d07ea0eca65b11beedb4ae196eb
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-07-18 08:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (54.56%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -4,15 +4,14 @@ public:
     {
         int i,j,n=nums.size();
         int count=0;
-        for(i=0;i<n;i++)
+        map <int,int> mpp;
+        for(i=0;i<nums.size();i++)
+            mpp[nums[i]]++;
+        
+        for( auto it : mpp )
         {
-            for(j=i+1;j<n;j++)
-            {
-                if( nums[i] == nums[j] )
-                {
-                    count++;
-                }
-            }
+            if( it.second > 1 )
+                count = count+ ((it.second)*(it.second-1)/2);
         }
         return count;
     }
