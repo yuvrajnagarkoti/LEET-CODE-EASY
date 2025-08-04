99faae6e9f646347c7a8d4d44750eebf107647fb
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-04 16:45:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (13.94%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+    public:
+        string toLowerCase(string s)
+        {
+            for(auto &c:s)
+            {
+                c = tolower(c);
+            }
+            return s;
+        }
+};
