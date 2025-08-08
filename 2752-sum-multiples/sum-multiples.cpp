04963a131001741b103f0f8b86374f8121110174
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-08 14:02:25 +0530

    Time: 2 ms (21.48%) | Memory: 8.6 MB (13.19%) - LeetSync

--- 2752-sum-multiples/sum-multiples.cpp
+++ 2752-sum-multiples/sum-multiples.cpp
@@ -4,11 +4,9 @@ public:
     {
         int sum=0;
         for(int i=3;i<=n;i++)
-        {
+        {   
             if(i%3==0 || i%5==0 || i%7==0)
-            {
                 sum+=i;
-            }
         }
         return sum;
     }
