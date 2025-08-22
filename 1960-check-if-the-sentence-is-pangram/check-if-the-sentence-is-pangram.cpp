b442bc6aa9dccbe1bec83a7187f30bf19f190ddd
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-22 08:14:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (7.27%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence)
+    {
+        unordered_set <char> s;
+        for(char c : sentence)
+        {
+            s.insert(c);
+        }
+        return s.size() == 26;
+    }
+};
