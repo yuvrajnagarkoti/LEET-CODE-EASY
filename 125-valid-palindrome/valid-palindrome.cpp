c2510fc9ff1a2227cc8146dc71e46aab83aa9a7e
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-24 15:15:28 +0530

    Time: 6 ms (7.53%) | Memory: 10.2 MB (28.20%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -5,10 +5,8 @@ public:
         string ans = "";
         for (int i = 0; i < s.length(); i++)
         {
-            if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
-                ans += tolower((unsigned char)s[i]);
-            else if( s[i]>='0' && s[i]<='9')
-                ans += tolower((unsigned char)s[i]);            
+            if ( (s[i]>='a'&&s[i]<='z') || (s[i]>='A'&&s[i] <= 'Z') || (s[i]>='0'&&s[i]<='9') )
+                ans += tolower(s[i]);          
         }
         int i=0,j=ans.length()-1;
         while(i <= j)
