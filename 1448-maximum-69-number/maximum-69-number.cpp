0f75571660afc4d22d74eb109169e0d5bcde92f4
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-26 22:45:29 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (72.50%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -1,24 +1,16 @@
-class Solution
-{
-    public:
+class Solution {
+public:
     int maximum69Number (int num)
     {
-        vector<int> ans;
-        int flag=0;
-        while(num > 0)
+        string a=to_string(num);
+        for(auto &c:a)
         {
-            ans.push_back(num%10);
-            num = num/10;
-        }
-        for(int i=ans.size()-1;i>=0;i--)
-        {
-            if(flag==0 && ans[i] == 6)
+            if(c=='6')
             {
-                ans[i]=9;
-                flag=1; 
+                c='9';
+                break;
             }
-            num=num*10+ans[i];
         }
-        return num;
+        return stoi(a);
     }
 };
