98c5976cb105db3c70244001f8072ebf5a805823
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-27 09:48:56 +0530

    Time: 0 ms (100.00%) | Memory: 32.9 MB (97.38%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> ans;
         for(int i=0;i<words.size();i++)
         {
-            for(int j=0;j<words[i].length();j++)
+            for(int j=0;j<words[i].size();j++)
             {
                 if(words[i][j] == x)
                 {
