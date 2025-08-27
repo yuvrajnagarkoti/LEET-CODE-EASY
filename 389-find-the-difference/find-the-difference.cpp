e1b009ca6cce8c1ac64c5a812f7497f2cc9107d2
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-27 15:26:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (29.47%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     char findTheDifference(string s, string t)
     {
-        int s1=0,s2=0;
-        for(int i=0;i<s.length();i++)
-        {
-            s1+=s[i];
-        }
+        int s1=0;
         for(int i=0;i<t.length();i++)
         {
-            s2+=t[i];
+            s1+=t[i];
         }
-        return (char)s2-s1;
+        for(int i=0;i<s.length();i++)
+        {
+            s1-=s[i];
+        }
+        return (char)s1;
     }
 };
