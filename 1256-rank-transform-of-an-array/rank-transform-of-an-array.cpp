5cab07509a0f0b72d5f2fe56863a9953dca14b48
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-29 10:12:59 +0530

    Time: 45 ms (40.55%) | Memory: 44 MB (52.80%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    vector<int> arrayRankTransform(vector<int>& arr)
-    {
-        set <int> s;
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        if (arr.empty())
+            return arr;
+        
+        vector<int> sorted = arr;
+        sort(sorted.begin(), sorted.end());
+        
+        unordered_map<int, int> rank;
+        int r = 1;
+        for (int i = 0; i < sorted.size(); i++) {
+            if (i == 0 || sorted[i] != sorted[i-1]) {
+                rank[sorted[i]] = r++;
+            }
+        }
+        
         for(int i=0;i<arr.size();i++)
         {
-            s.insert(arr[i]);
-        }
-        map<int,int> mpp;
-        int k=1;
-        for(auto it : s)
-        {
-            mpp[it]=k;
-            k++;
-        }
-        for(int i=0;i<arr.size();i++)
-        {
-            arr[i] = mpp[arr[i]];
+            arr[i] = rank[arr[i]];
         }
         return arr;
     }
