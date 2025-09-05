8a0de9e73e8c5ef3fc968702d28103fb8dd7b1c9
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-09-05 21:52:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (55.50%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.cpp
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -6,14 +6,10 @@ class Solution
         int i=0,j=s.length();
         while( i<j )
         {
-            while( i<j && (s[i]<'a'||s[i]>'z') && (s[i]<'A'||s[i]>'Z') ) 
-            {
+            while( i<j && (s[i]<'a'||s[i]>'z') && (s[i]<'A'||s[i]>'Z') )
                 i++;
-            }
             while( j>i && (s[j]<'a'||s[j]>'z') && (s[j]<'A'||s[j]>'Z') )
-            {
                 j--;
-            }
             char t=s[i];
             s[i++]=s[j];
             s[j--]=t;
