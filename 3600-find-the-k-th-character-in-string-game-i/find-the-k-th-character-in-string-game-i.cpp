f250b400eb1591697d611c8f23b144d360e3a55d
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-10-13 20:43:20 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (11.37%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -1,20 +1,26 @@
-class Solution {
-public:
+class Solution
+{
+    public:
+    void tillk(string &word,int k)
+    {
+        string w="";
+        if(word.length() >= k)
+            return;
+        for(int i=0;i<word.length();i++)
+        {
+            if( word[i] == 'z')
+                w+='a';
+            else
+                w+= word[i]+1;
+        }
+        word=word+w;
+        tillk(word,k);
+    }
+
     char kthCharacter(int k)
     {
-        string word = "a";
-        while( word.length() < k)
-        {
-            string w="";
-            for(int i=0;i<word.length();i++)
-            {
-                if(word[i] == 'z')
-                    w += 'a';
-                else
-                    w += word[i]+1;
-            }
-            word=word+w;
-        }
+        string word="a";
+        tillk(word,k);
         return word[k-1];
     }
 };
