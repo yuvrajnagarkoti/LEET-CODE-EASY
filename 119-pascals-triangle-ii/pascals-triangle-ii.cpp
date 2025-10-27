81c7e954261fe3a9cbc6270992fbcec53e8a91d9
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-10-27 17:36:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (74.82%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    vector<int> getRow(int rowi)
+    vector<int> getRow(int rowIndex)
     {
-        vector<vector<int>> ans(rowi + 1);
-        ans[0].push_back(1);
-        int i=1;
-        while( i<=rowi)
+        vector<int> row = {1};
+        for (int i = 1; i <= rowIndex; i++)
         {
-            ans[i].push_back(1);
-
-            int n=ans[i-1].size();
-            for(int j=1;j<n;j++)
+            vector<int> newRow(i + 1, 1);
+            for (int j = 1; j < i; j++)
             {
-                int temp = ans[i-1][j-1] + ans[i-1][j];
-                ans[i].push_back(temp);
+                newRow[j] = row[j - 1] + row[j];
             }
-            ans[i].push_back(1);
-            i++;
+            row = newRow;
         }
-        return ans[rowi];
+        return row;
     }
 };
