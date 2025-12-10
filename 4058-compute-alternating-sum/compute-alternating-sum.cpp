d150dd7038ae335e8c279771511d5d86a21b3cf6
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-12-10 07:49:35 +0530

    Time: 0 ms (100.00%) | Memory: 32.7 MB (12.52%) - LeetSync

--- 4058-compute-alternating-sum/compute-alternating-sum.cpp
+++ 4058-compute-alternating-sum/compute-alternating-sum.cpp
@@ -3,19 +3,12 @@ public:
     int alternatingSum(vector<int>& nums)
     {
         int sum=0;
-        int flag=0;
         for(int i=0;i<nums.size();i++)
         {
-            if(flag==0)
-            {
+            if(i%2 == 0)
                 sum=sum+nums[i];
-                flag=1;
-            }
             else
-            {
                 sum=sum-nums[i];
-                flag=0;
-            }    
         }
         return sum;
     }
