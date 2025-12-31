f6c3a8d6476507c090ab0041e9a66ab45cb4d4d9
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-12-31 17:57:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (37.55%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,26 +2,37 @@ class Solution {
 public:
     bool isValid(string s)
     {
-        stack <char> stk;
-        for(char ch : s)
+        stack<char> stk;
+        for(int i=0;i<s.length();i++)
         {
-            if (ch=='(' ||  ch=='{' || ch=='[' )
+            if(stk.empty())
             {
-                stk.push(ch);
+                if(s[i] == ')' || s[i] == ']' || s[i] == '}')
+                    return false;
+                else
+                    stk.push(s[i]);
             }
             else
             {
-                if(stk.empty())
-                    return false;
-                if ((ch==')' && stk.top()!='(') ||
-                    (ch=='}' && stk.top()!='{') ||
-                    (ch==']' && stk.top()!='['))
+                char temp=stk.top();
+                if(s[i] == '(' || s[i] == '[' || s[i] =='{')
+                {
+                    stk.push(s[i]);
+                }
+                else if( (s[i] == ')' && temp == '(') || 
+                    (s[i] == ']' && temp == '[') ||
+                    (s[i] =='}'  && temp =='{') )
+                {
+                        stk.pop();
+                }
+                else
                 {
                     return false;
                 }
-                stk.pop();
             }
         }
-        return stk.empty();
+        if(!stk.empty())
+            return false;
+        return true;
     }
 };
