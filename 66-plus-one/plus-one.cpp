1be8361cebec170ed3f8bc54c5ad218cf49d62c5
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-01-01 08:26:30 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (96.43%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits)
     {
-        stack<int> stk;
-        vector<int> ans;
-        for(int i=0;i<digits.size();i++)
-        {
-            stk.push(digits[i]);
-        }
         int flag=1;
-        while(!stk.empty())
+        vector<int> ans;
+        for(int i=digits.size()-1;i>=0;i--)
         {
-            int temp = stk.top();
-            stk.pop();
-            if(flag == 1)
+            int temp = digits[i];
+            if(flag==1)
             {
-                temp=temp+1;
-                flag=0;
+                temp++;
+                flag=0;     
             }
             if(temp == 10)
             {
@@ -26,9 +20,7 @@ public:
             ans.push_back(temp);
         }
         if(flag==1)
-        {
             ans.push_back(1);
-        }
         reverse(ans.begin(),ans.end());
         return ans;
     }
