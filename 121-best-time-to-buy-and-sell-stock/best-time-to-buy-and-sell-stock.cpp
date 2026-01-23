f8c150bd49104ff74a597b521e0d55622654ddbf
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-01-23 19:50:27 +0530

    Time: 1 ms (35.97%) | Memory: 101.3 MB (5.23%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -5,6 +5,7 @@ public:
         int n=prices.size();
         vector<int> dp(n,0);
         int mini = prices[0];
+        int ans=0;
         for(int i=1;i<n;i++)
         {
             dp[i] = mini;
@@ -12,10 +13,6 @@ public:
             {
                 mini=prices[i];
             }
-        }
-        int ans=0;
-        for(int i=1;i<n;i++)
-        {
             if(prices[i]-dp[i] > ans)
             {
                 ans=prices[i]-dp[i];
