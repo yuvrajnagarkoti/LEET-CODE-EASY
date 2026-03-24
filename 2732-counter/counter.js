4a7caef1d7819f4739e7ad78d0e7d4468d2a0093
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-03-24 10:07:24 +0530

    Time: 36 ms (87.60%) | Memory: 53.8 MB (38.14%) - LeetSync

---
+++ 2732-counter/counter.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    var i = -1;
+    return function()
+    {
+        i++;
+        return n+i;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
