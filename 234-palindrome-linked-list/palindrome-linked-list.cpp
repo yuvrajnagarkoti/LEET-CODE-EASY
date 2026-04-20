de9bd40b7cd57b38b7e7280ecd486ea4bc574107
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-04-21 00:19:24 +0530

    Time: 1 ms (67.66%) | Memory: 118 MB (81.72%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,20 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution
 {
 public:
+
+    ListNode * rev(ListNode *slow)
+    {
+        ListNode *prev=slow;
+        ListNode *nxt=slow->next;
+        prev->next = NULL;
+        while(nxt != NULL)
+        {
+            ListNode *temp = nxt->next;
+            nxt->next = prev;
+            prev=nxt;
+            nxt = temp;
+        }
+        return prev;
+    }
+
     bool isPalindrome(ListNode* head)
     {
-        vector<int> digits;
-        while (head != NULL)
+        ListNode *slow=head;
+        ListNode *fast=head;
+        ListNode *prev=slow;
+        while(fast != NULL && fast->next != NULL)
         {
-            digits.push_back(head->val);
-            head = head->next;
+            prev=slow;
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        int i = 0, j = digits.size() - 1;
-        while (i < j) {
-            if (digits[i] != digits[j])
+        slow = rev(slow);
+        prev->next=NULL;
+        fast=head;
+        while(fast != NULL && slow != NULL)
+        {
+            if(fast->val != slow->val)
                 return false;
-            i++;
-            j--;
+            
+            fast=fast->next;
+            slow=slow->next;
         }
         return true;
     }
