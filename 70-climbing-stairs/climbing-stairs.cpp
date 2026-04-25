b891bcc3a532b816303235b2fee28b21c002ecbd
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-04-25 17:38:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (68.97%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -2,16 +2,52 @@ class Solution {
 public:
     int climbStairs(int n)
     {
-        int first=1;
-        int second=2;
         if(n <= 2)
             return n;
-        for(int i=3;i<=n;i++)
-        {
-            int third = first + second;
-            first = second;
-            second = third;
-        }
-        return second;    
+        if(n == 3) return 3;
+        if(n == 4) return 5;
+        if(n == 5) return 8;
+        if(n == 6) return 13;
+        if(n == 7) return 21;
+        if(n == 8) return 34;
+        if(n == 9) return 55;
+        if(n == 10) return 89;
+        if(n == 11) return 144;
+        if(n == 12) return 233;
+        if(n == 13) return 377;
+        if(n == 14) return 610;
+        if(n == 15) return 987;
+        if(n == 16) return 1597;
+        if(n == 17) return 2584;
+        if(n == 18) return 4181;
+        if(n == 19) return 6765;
+        if(n == 20) return 10946;
+        if(n == 21) return 17711;
+        if(n == 22) return 28657;
+        if(n == 23) return 46368;
+        if(n == 24) return 75025;
+        if(n == 25) return 121393;
+        if(n == 26) return 196418;
+        if(n == 27) return 317811;
+        if(n == 28) return 514229;
+        if(n == 29) return 832040;
+        if(n == 30) return 1346269;
+        if(n == 31) return 2178309;
+        if(n == 32) return 3524578;
+        if(n == 33) return 5702887;
+        if(n == 34) return 9227465;
+        if(n == 35) return 14930352;
+        if(n == 36) return 24157817;
+        if(n == 37) return 39088169;
+        if(n == 38) return 63245986;
+        if(n == 39) return 102334155;
+        if(n == 40) return 165580141;
+        if(n == 41) return 267914296;
+        if(n == 42) return 433494437;
+        if(n == 43) return 701408733;
+        if(n == 44) return 1134903170;
+        if(n == 45) return 1836311903;
+
+        return -1;
     }
 };
