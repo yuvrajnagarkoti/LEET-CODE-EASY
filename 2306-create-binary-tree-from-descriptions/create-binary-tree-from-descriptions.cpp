c550020779ada358a14bef689de3a69f70a41937
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-06-07 06:31:26 +0530

    Time: 291 ms (20.08%) | Memory: 257.8 MB (93.02%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& desc)
+    {
+        sort(desc.begin(),desc.end());
+        map<int,TreeNode*> mpp;
+        for(int i=0;i<desc.size();i++)
+        {
+            int parent = desc[i][0];
+            int child = desc[i][1];
+            int isleft = desc[i][2];
+            if(mpp.find(parent) == mpp.end())
+                mpp[parent] = new TreeNode(parent);
+            if(mpp.find(child) == mpp.end())
+                mpp[child] = new TreeNode(child);
+            
+            if(isleft)
+                mpp[parent]->left = mpp[child];
+            else
+                mpp[parent]->right = mpp[child];
+        }
+        for(int i=0;i<desc.size();i++)
+        {
+            int child = desc[i][1];
+            mpp.erase(child);
+        }
+
+        auto it = mpp.begin();
+        return it->second;
+    }
+};
