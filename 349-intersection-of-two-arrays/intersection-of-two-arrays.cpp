02b5f48e2dc4de5375cac3238f450b21c6cb6daa
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-08-29 08:46:34 +0530

    Time: 7 ms (10.19%) | Memory: 14.3 MB (57.04%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2)
     {
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        int i,l1=0,l2=0;
-        vector<int> ans;
-        while( l1<nums1.size() && l2<nums2.size() )
+        map<int,int> mpp;
+        int n=nums1.size(),m=nums2.size();
+        for(int i=0;i<n;i++)
         {
-            if(nums1[l1] == nums2[l2])
-            {
-                ans.push_back(nums1[l1++]);
-                while(l1<nums1.size() && nums1[l1] == nums1[l1-1] )
-                    l1++;
-            }
-            else if( nums1[l1] > nums2[l2])
-                l2++;
-            else
-                l1++;
+            mpp[nums1[i]]++;
         }
+        vector<int> ans;
+        for(int i=0;i<m;i++)
+        {
+            if(mpp.find(nums2[i])!=mpp.end() && mpp[nums2[i]] > 0)
+            {
+                ans.push_back(nums2[i]);
+                mpp.erase(nums2[i]);
+            }
+        }
+
         return ans;
     }
 };
