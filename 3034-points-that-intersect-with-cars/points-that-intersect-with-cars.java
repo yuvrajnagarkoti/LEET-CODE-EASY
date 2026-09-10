0f08a897140a8db8bea4a9e6e55dc9d4cc88033d
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-09-10 10:24:34 +0530

    Time: 1 ms (100.00%) | Memory: 46.8 MB (5.78%) - LeetSync

--- 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
@@ -1,22 +1,25 @@
 class Solution {
     public int numberOfPoints(List<List<Integer>> nums)
     {
-        int[] count = new int[101];
+        int []count = new int[102];
         for(int i=0;i<nums.size();i++)
         {
             int low=nums.get(i).get(0);
             int high=nums.get(i).get(1);
-            while(low <= high)
-            {
-                count[low]=1;
-                low++;
-            }
+
+            count[low]++;
+            count[high+1]--;
         }
         int ans=0;
-        for(int i=0;i<101;i++)
+        int pre=0;
+        for(int i=1;i<101;i++)
         {
-            if(count[i] == 1)
+            pre = pre + count[i];
+            count[i] = pre;
+            if(count[i] >= 1)
+            {
                 ans++;
+            }
         }
         return ans;
     }
