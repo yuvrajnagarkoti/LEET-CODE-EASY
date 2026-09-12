14742995ca81134d08f32e604b11bbec3408c413
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-09-12 18:15:29 +0530

    Time: 2 ms (47.78%) | Memory: 13.8 MB (5.44%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -4,36 +4,28 @@ public:
     {
         stack<pair<char,int>> st;
         int n=s.length();
+        int ans=0;
+
+        st.push({')',-1});
+
         for(int i=0;i<n;i++)
         {
             if(s[i] == '(')
                 st.push({'(',i});
             else
             {
-                if(st.empty() || st.top().first == ')')
+                if(st.top().first == ')')
+                {
                     st.push({')',i});
-                else 
+                }
+                else
+                {
                     st.pop();
+
+                    ans = max(ans,i-st.top().second);
+                }
             }
         }
-        if(st.empty())
-            return n;
-        
-        vector<int> ans;
-        ans.push_back(n);
-        while( ! st.empty())
-        {
-            ans.push_back(st.top().second);
-            st.pop();
-        }
-        ans.push_back(-1);
-
-        int a=0;
-        for(int i=0;i<ans.size()-1;i++)
-        {
-            a = max(a,ans[i]-ans[i+1]-1);
-        }
-
-        return a;
+        return ans;
     }
 };
